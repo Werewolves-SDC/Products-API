8e980691a7e0426c2b3fee99d8f31c0f2b5cde13
Cole Bienek <colebienek@gmail.com>
2022-11-02 13:26:16 -0700

    establish db connection, server active

--- dataTransform/ProductsTransformer.js
+++ dataTransform/ProductsTransformer.js
@@ -1,4 +1,4 @@
 const csv = require("csv-parser");
 const createCsvStringifier = require("csv-writer").createObjectCsvStringifier; const fs = require('fs');
 const Transform = require("stream").Transform;
-
+const Products = require('../csv_files/product.csv');
