0a82c8e7004bb84ecfdd1a5fd99410cf8feb8e68
Cole Bienek <colebienek@gmail.com>
2022-11-02 10:04:06 -0700

    Initial commit, beginning data build

---
+++ dataTransform/ProductsTransformer.js
@@ -0,0 +1,4 @@
+const csv = require("csv-parser");
+const createCsvStringifier = require("csv-writer").createObjectCsvStringifier; const fs = require('fs');
+const Transform = require("stream").Transform;
+
