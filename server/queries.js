fb9ae5dac0a46906c0af67d0e8b41b1b54f00c71
Cole Bienek <colebienek@gmail.com>
2022-11-10 16:44:37 -0800

    refactor heavy styles query to avoid looping to produce api

--- server/queries.js
+++ server/queries.js
@@ -29,4 +29,50 @@ module.exports = {
   INNER JOIN skus
   ON styles.style_id = skus.style_id
   WHERE styles.product_id= $1`,
+
+  product: `
+  SELECT json_build_object('productsInfo', jsonb_build_object(
+    'id', products.product_id,
+    'name', products.name,
+    'slogan', products.slogan,
+    'description', products.description,
+    'category', products.category,
+    'default_price', products.default_price,
+    'features', (json_build_array(json_build_object('feature',features.feature, 'value',features.value)))
+  )
+  )
+  FROM products
+  INNER JOIN features
+  ON products.product_id = features.product_id
+  WHERE products.product_id= $1
+  `,
+
+  styleDetails: `
+   SELECT
+   styles.style_id AS "style_id",
+   styles.name,
+   styles.original_price,
+   styles.sale_price,
+   styles.default AS "default?",
+   (
+     SELECT jsonb_agg(jsonb_build_object(
+       'thumbnail_url', photos.thumbnail_url,
+       'url', photos.url
+       )) AS photos
+     FROM photos
+     WHERE styles.style_id = photos.style_id
+   ),
+   (
+     SELECT jsonb_agg(jsonb_build_object(
+       'id', skus.id,
+       'quantity', skus.quantity,
+       'size', skus.size
+     )) AS skus
+     FROM skus
+     WHERE styles.id = skus.style_id
+   )
+   FROM styles
+   WHERE styles.product_id= $1
+   `,
+
 };
